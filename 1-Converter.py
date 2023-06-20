cf9ee17020997809dd36b3631dfe4fdc967da578
wayne <wayne_lau@aiap.sg>
2023-06-20 13:12:33 +0800

    fix(converter): copy text

--- 1-Converter.py
+++ 1-Converter.py
@@ -6,7 +6,7 @@ def count_bytes(text, encoding='utf-8'):
     return len(text.encode(encoding))
 
 # Title
-st.title('Unicode to ASCII converter')
+st.title('Unicode to ASCII text cleaner')
 
 # Text input
 user_input = st.text_area("Enter/paste your text here")
@@ -15,10 +15,7 @@ user_input = st.text_area("Enter/paste your text here")
 if user_input:
     cleaned_text = unidecode_expect_ascii(user_input)
     st.write("Your text had", count_bytes(user_input), "bytes")
-    st.write("Here's the ASCII version of your text:")
-    st.text(cleaned_text)
+    st.write("""Here's the clean version of your text:  
+    You can select all the text to copy it. """)
+    st.text_area("Cleaned Text", cleaned_text, height=150)
     st.write("It now has", count_bytes(cleaned_text), "bytes")
-
-    # Add a button to allow copying to clipboard
-    if st.button('Copy to clipboard'):
-        st.copied_to_clipboard(cleaned_text)
